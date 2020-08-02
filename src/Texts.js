fc67201fc0507d58199ee675b25bc2391dd3571a
Vojtěch Zicha <vojtech@zicha.name>
2020-08-02 11:54:43 +0200

    Fix linting errors

--- src/Texts.js
+++ src/Texts.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Table, SideSheet, Paragraph } from 'evergreen-ui'
+import { Table } from 'evergreen-ui'
 import { get } from 'axios'
 
 const conf = {
   0: 'https://1drv.ms/u/s!Al_GOxp7elhTluRMAwqNF0Hs5CGepQ',
-  1: 'https://1drv.ms/u/s!Al_GOxp7elhTluRPuaflXqAfxSL8EA',
+  1: 'https://1drv.ms/u/s!Al_GOxp7elhTluRPuaflXqAfxSL8EA'
 }
 
 export const Texts = ({ name }) => {
@@ -16,23 +16,26 @@ export const Texts = ({ name }) => {
       const res = (await get(uri)).data
       setItems(
         res.children
-          .filter((i) => !i.name.includes('txt'))
-          .map((i) => ({ name: 'notready', id: i.id, description: 'nahrává...', url: i['@content.downloadUrl'] }))
+          .filter(i => !i.name.includes('txt'))
+          .map(i => ({
+            name: 'notready',
+            id: i.id,
+            description: 'nahrává...',
+            url: i['@content.downloadUrl']
+          }))
       )
-      res.children.forEach(async (i) => {
-        if (res.children.filter((ii) => ii.name === `${i.name}.txt`).length > 0) {
-          const inner = res.children.find((ii) => ii.name === `${i.name}.txt`)
+      res.children.forEach(async i => {
+        if (res.children.filter(ii => ii.name === `${i.name}.txt`).length > 0) {
+          const inner = res.children.find(ii => ii.name === `${i.name}.txt`)
           const description = (await get(inner['@content.downloadUrl'])).data
-          setItems((items) => [
-            ...items.filter((ii) => ii.id !== i.id),
-            { name: 'ready', id: i.id, description, url: i['@content.downloadUrl'] },
+          setItems(items => [
+            ...items.filter(ii => ii.id !== i.id),
+            { name: 'ready', id: i.id, description, url: i['@content.downloadUrl'] }
           ])
         }
       })
     })()
   }, [name])
-
-  //https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL2YvcyFBdHVBTV9OYWN3VmFoaUZwdU1HU19CaVFDd1d1/root?expand=children
 
   return (
     <>
